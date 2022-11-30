60b944162cfe05c3d28ceef58cb3eab3b2083906
rockox1992 <94861200+rockox1992@users.noreply.github.com>
2022-11-30 19:19:23 +0300

    Add files via upload

---
+++ hw_2_3.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class hw_2_3 {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите любой текст кроме пустой строки");
+        try {
+            String result = reader.readLine();
+            if(result.equals("")) throw new RuntimeException("Пустую строку вводить нельзя");
+
+            System.out.println(result);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
